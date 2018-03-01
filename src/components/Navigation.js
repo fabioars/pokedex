96e8f23906014864b7ff6131bb666c1c9d3b8706
Fabio Soares <fabioars@live.com>
2018-03-01 09:50:02 -0300

    Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Icon from './navigation/Icon';
 
 class Navigation extends Component {
     render() {
         return (
             <div className="Navigation">
                 <div className="container Navigation__container">
-                    Navigation
+                    <ul className="Navigation__list">
+                        <li className="Navigation__item">
+                            <Link to="/" className="Navigation__link"> 
+                                <Icon src={require('./navigation/pokeballs.svg')} />
+                            </Link>
+                        </li>
+                        <li className="Navigation__item">
+                            <Link to="/pokemon" className="Navigation__link">
+                                <Icon src={require('./navigation/pokecoin.svg')} />
+                            </Link>
+                        </li>
+                        <li className="Navigation__item">
+                            <Link to="/search" className="Navigation__link">
+                                <Icon src={require('./navigation/compass.svg')} />
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
---
+++ src/components/navigation/Icon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Icon = ({ src, alt }) => (
+    <img src={src} className="Navigation__icon" alt={alt} />
+);
+
+export default Icon;
