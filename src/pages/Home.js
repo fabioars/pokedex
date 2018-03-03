14e526c65f2db144299699fb95e4b7195462b207
Fabio Soares <fabioars@live.com>
2018-03-03 18:25:54 -0300

    Lista pokémons na home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import NoPokemon from './home/NoPokemon';
+import Item from '../components/Item';
 
 class Home extends Component {
     render() {
+        const { list } = this.props;
+
         return (
             <div className="App__content Home">
                 <div className="container Home__container">
-                    <NoPokemon />
+                    {this.props.count === 0 ? <NoPokemon /> : (
+                        <ul className="Home__list">
+                            {list.map(pokemon => {
+                                return (
+                                    <Item key={pokemon.id} number={pokemon.id}>
+                                        {pokemon.name}
+                                    </Item>
+                                );
+                            })}
+                        </ul>
+                    )}
                 </div>
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = state => {
+    return {
+        count: state.pokedex.list.length,
+        list: state.pokedex.list
+    };
+};
+
+export default connect(mapStateToProps)(Home);
