82379f67fa8d8257f8f5b84b1ca7c77dd644b954
Fabio Soares <fabioars@live.com>
2018-03-04 20:39:11 -0300

    Exibe stats

--- src/pages/pokemon/Profile.js
+++ src/pages/pokemon/Profile.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
 class Profile extends Component {
-
     onRelease = e => {
         e.preventDefault();
         this.props.onRelase(this.props.pokemon.id);
-    }
+    };
 
     render() {
         const { pokemon } = this.props;
@@ -14,13 +13,30 @@ class Profile extends Component {
             <div className="Profile">
                 <div className="Profile__info">
                     <div className="Profile__sprite">
-                        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                        <img
+                            src={pokemon.sprites.front_default}
+                            alt={pokemon.name}
+                        />
                     </div>
                     <div className="Profile__action">
                         <span className="Profile__name">{pokemon.name}</span>
-                        <button className="Button" onClick={this.onRelease}>Release</button>
+                        <button className="Button" onClick={this.onRelease}>
+                            Release
+                        </button>
                     </div>
                 </div>
+                <table className="Profile__table">
+                    <tbody>
+                        {[...pokemon.stats].reverse().map(stats => {
+                            return (
+                                <tr key={stats.stat.name}>
+                                    <td>{stats.stat.name.toUpperCase()}</td>
+                                    <td align="right">{stats.base_stat}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
             </div>
         );
     }
