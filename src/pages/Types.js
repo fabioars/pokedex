098cfff09c34525b4f0d28865d774abb96dd5def
Fabio Soares <fabioars@live.com>
2018-03-05 00:03:04 -0300

    Lista pokémon por tipo

---
+++ src/pages/Types.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Item from '../components/Item';
+import Loading from '../components/Loading';
+
+class Types extends Component {
+    state = {
+        error: false,
+        loading: false,
+        list: []
+    };
+
+    componentWillMount() {
+        this.loadContent();
+    }
+
+    async loadContent() {
+        const { name } = this.props.match.params;
+        this.setState({ loading: true });
+
+        const result = await axios.get(
+            `https://pokeapi.co/api/v2/type/${name}`
+        );
+
+        if (result) {
+            this.setState({
+                list: result.data.pokemon,
+                loading: false,
+                error: false
+            });
+        } else {
+            this.setState({
+                loading: false,
+                error: true
+            });
+        }
+    }
+
+    renderList() {
+        return this.state.list.map(pokemon => {
+            const id = pokemon.url.split('/').reverse()[1];
+
+            return (
+                <Item key={id} number={id}>
+                    {pokemon.name}
+                </Item>
+            );
+        });
+    }
+
+    render() {
+        if(this.state.error) {
+            this.loadContent();
+        }
+
+        return (
+            <div className="App__content Types">
+                <div className="container">
+                    <ul className="Types__list">
+                        {this.state.loading ? <Loading /> : null}
+                        {this.renderList()}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Types;
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import All from './pages/All';
 import Pokemon from './pages/Pokemon';
+import Types from './pages/Types';
 
 const Router = () => (
     <BrowserRouter>
@@ -13,6 +14,7 @@ const Router = () => (
             <Route exact path="/" component={Home} />
             <Route exact path="/pokemon" component={All} />
             <Route exact path="/pokemon/:id" component={Pokemon} />
+            <Route exact path="/type/:name" component={Types} />
             <Navigation />
         </div>
     </BrowserRouter>
